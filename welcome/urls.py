d8c60c5252f2cf5c2ae1fb20d54af2ae85f88692
nebula1989 <benwalterscoding@gmail.com>
2022-05-30 17:23:22 -0700

    setup urls and links

--- welcome/urls.py
+++ welcome/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
-    path('repair_services', views.repair_services, name='repair_services')
+    path('repair_services', views.repair_services, name='repair_services'),
+    path('pricing_tables', views.pricing_tables, name='pricing_tables'),
+    path('team', views.team, name='team'),
+    path('register', views.register, name='register'),
+    path('faq', views.faq, name='faq'),
 ]
--- welcome/views.py
+++ welcome/views.py
@@ -13,5 +13,21 @@ def repair_services(request):
     return render(request, 'all-services-repair.html')
 
 
+def pricing_tables(request):
+    return render(request, 'pricing-tables.html')
+
+
+def team(request):
+    return render(request, 'team.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def faq(request):
+    return render(request, 'faq.html')
+
+
 def error_404(request):
     return render(request, '404.html')
