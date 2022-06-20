8ee12c018c14a99c4f1c1af79174c39d1e124ffa
nebula1989 <benwalterscoding@gmail.com>
2022-06-20 12:33:28 -0700

    customized template, added more content sections

--- welcome/views.py
+++ welcome/views.py
@@ -19,6 +19,20 @@ def repair_services(request):
     return render(request, 'all-services-repair.html', context)
 
 
+def tech_services(request):
+    context = {
+        "top_banner_name": "Technology Services",
+    }
+    return render(request, 'all-services-technology.html', context)
+
+
+def laptop_repair(request):
+    context = {
+        "top_banner_name": "Laptop Repair",
+    }
+    return render(request, 'laptop-repair.html', context)
+
+
 def pricing_tables(request):
     context = {
         "top_banner_name": "Prices",
