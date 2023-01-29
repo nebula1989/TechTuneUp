6165455bf6acb215fc2657fdd664752281bad085
nebula1989 <benwalterscoding@gmail.com>
2023-01-28 19:38:30 -0800

    updated prod settings

--- techtuneup/settings/prod.py
+++ techtuneup/settings/prod.py
@@ -11,8 +11,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'OPTIONS': {
-            'service': 'techtuneup',
-            'passfile': '.my_pgpass',
+            'service': '',
+            'passfile': '',
         },
     }
 }
@@ -20,9 +20,9 @@ DATABASES = {
 
 # default static files settings for PythonAnywhere.
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
-MEDIA_ROOT = '/home/bwalters89/techtuneup/media'
-MEDIA_URL = '/media/'
-STATIC_ROOT = '/home/bwalters89/techtuneup/static'
+MEDIA_ROOT = ''
+MEDIA_URL = ''
+STATIC_ROOT = '/home/ubuntu/TechTuneUp/static'
 STATIC_URL = '/static/'
 
 TIME_ZONE = 'UTC'
