3d252c7b6406bcdf3aab2d64271f50542f2f809a
nebula1989 <benwalterscoding@gmail.com>
2023-03-05 18:02:33 -0800

    fixed settings git issue

--- techtuneup/settings/prod.py
+++ techtuneup/settings/prod.py
@@ -9,29 +9,16 @@ ALLOWED_HOSTS = ['www.techtuneup.org', 'techtuneup.org']
 
 DATABASES = {
     'default': {
-<<<<<<< HEAD
-        'ENGINE': 'django.db.backends.postgresql',
-        'OPTIONS': {
-            'service': '',
-            'passfile': '',
-        },
-=======
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
->>>>>>> fix_recaptcha
     }
 }
 
 
 # default static files settings for PythonAnywhere.
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
-<<<<<<< HEAD
-MEDIA_ROOT = ''
-MEDIA_URL = ''
-=======
 MEDIA_ROOT = '/home/ubuntu/TechTuneUp/media'
 MEDIA_URL = '/media/'
->>>>>>> fix_recaptcha
 STATIC_ROOT = '/home/ubuntu/TechTuneUp/static'
 STATIC_URL = '/static/'
 
