74a8c790a07348fc16910a7a88e2f8170db5ff9c
nebula1989 <benwalterscoding@gmail.com>
2023-03-12 14:38:51 -0700

    optimized page for seo

--- welcome/urls.py
+++ welcome/urls.py
@@ -6,16 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('services/', views.services, name='services'),
-    path('repair', views.repair_services, name='repair_services'),
-    path('repair/pc/', views.pc_repair, name='pc_repair'),
-    path('repair/mobile/', views.mobile_repair, name='mobile_repair'),
-    path('repair/tablet/', views.tablet_repair, name='tablet_repair'),
-    path('repair/game-console/', views.gaming_repair, name='gaming_repair'),
-
-    path('tech_services/', views.tech_services, name='tech_services'),
-    path('website-development', views.app_dev, name='app_dev'),
-    path('team/', views.team, name='team'),
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
     path('faq/', views.faq, name='faq'),
 ]
--- welcome/views.py
+++ welcome/views.py
@@ -22,76 +22,6 @@ def services(request):
     return render(request, 'services.html', context)
 
 
-def repair_services(request):
-    context = {
-        "top_banner_name": "Repair Services",
-    }
-    return render(request, 'repair-services-base.html', context)
-
-
-def tech_services(request):
-    context = {
-        "top_banner_name": "Technology Services",
-    }
-    return render(request, 'technology-services-base.html', context)
-
-
-def app_dev(request):
-    context = {
-        "top_banner_name": "Application Development",
-    }
-    return render(request, 'development-services-base.html', context)
-
-
-def pc_repair(request):
-    context = {
-        "top_banner_name": "PC Repair",
-    }
-    return render(request, 'pc-repair.html', context)
-
-
-def mobile_repair(request):
-    context = {
-        "top_banner_name": "Mobile Repair",
-    }
-    return render(request, 'mobile-repair.html', context)
-
-
-def tablet_repair(request):
-    context = {
-        "top_banner_name": "Tablet Repair",
-    }
-    return render(request, 'tablet-repair.html', context)
-
-
-def gaming_repair(request):
-    context = {
-        "top_banner_name": "Gaming Console Repair",
-    }
-    return render(request, 'game-console-repair.html', context)
-
-
-def team(request):
-    context = {
-        "top_banner_name": "Meet the Team",
-    }
-    return render(request, 'team.html', context)
-
-
-def register(request):
-    context = {
-        "top_banner_name": "Sign Up",
-    }
-    return render(request, 'register.html', context)
-
-
-def login(request):
-    context = {
-        "top_banner_name": "Login",
-    }
-    return render(request, 'login.html', context)
-
-
 def faq(request):
     context = {
         "top_banner_name": "FAQ",
@@ -104,5 +34,3 @@ def error_404(request):
         "top_banner_name": "404 Page Not Found",
     }
     return render(request, '404.html', context)
-
-
